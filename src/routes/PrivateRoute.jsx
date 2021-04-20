32ef1a36e218313ec95a34ae489d7f32aed4ce73
Dinh Son Hai <sdinhhai@gmail.com>
2021-04-20 11:36:46 +0700

    Add custom private route

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Spinner } from '../components/Spinner';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      loading ? (
+        <Spinner width="200px" />
+      ) : isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
+
